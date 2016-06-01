135f9179ca36ac6808ce46b128d88f500a4ffd73
Avi Goldman <avrahamymgoldman@gmail.com>
2016-06-01 12:28:01 -0400

    cleaned up functions and added support for topics in each endpoint

--- js/redirect.js
+++ js/redirect.js
@@ -1,35 +1,52 @@
 (function($) {
-	var PATH = location.pathname;
-	var HASH = location.hash;
-	var URL_SPACE = '_';	
+	var PATH = location.pathname, HASH = location.hash,  PATH_SPACE = '_', HASH_SPACE = '-';	
 
-	// API page with some old hash
+	// check if we are on the API page with an Apiary hash
 	if (PATH === '/api/' && (HASH.indexOf('introduction') >= 0 || HASH.indexOf('reference') >= 0)) {
 		var newPath = convertHashToPath(HASH);
 
-		redirect('http://ewandennis.github.io/staticapi' + newPath);
+		redirect('http://ewandennis.github.io/staticapi' + PATH + newPath);
+	}
+
+
+
+	function redirect(url) {
+		console.log(url);
+		// window.location.replace(url);
+	}
+
+	function convertHashToPath(hash) {
+		var hashPath = hash.substring(2);
+		var type     = getType(hashPath);
+		var endpoint = getEndpoint(hashPath);
+		var method   = getMethod(hashPath);
+
+		// add endpoint
+		var path = (endpoint + PATH_SPACE + 'api.html').replace(/-|_/g, PATH_SPACE);
+
+		// add method if its a reference
+		if (type === 'reference' && method.length > 0)
+			path += ('#' + endpoint + HASH_SPACE + method).replace(/-|_/g, HASH_SPACE);
+
+		return path;
+	}
+
+	function getType(hashPath) {
+		var parts = hashPath.split('/');
+
+		return parts.length >= 1 ? parts[0] : '';
+	}
+
+	function getEndpoint(hashPath) {
+		var parts = hashPath.split('/');
+
+		return parts.length >= 2 ? parts[1] : '';
+	}
+
+	function getMethod(hashPath) {
+		var parts = hashPath.split('/');
+
+		return parts.length >= 3 ? parts[2] : '';
 	}
 
 })(jQuery);
-
-
-function redirect(url) {
-	console.log(url);
-	// window.location.replace(url);
-}
-
-function convertHashToPath(hash) {
-	var hashPath = hash.substring(2);
-	
-	var endpoint = getEndpoint(hashPath);
-
-	return (PATH + endpoint).replace(/-|_/, URL_SPACE);
-}
-
-function getEndpoint(hashPath) {
-	var firstSlash  = hashPath.indexOf('/') + 1;
-	var secondSlash = hashPath.substring(firstSlash).indexOf('/');
-	    secondSlash = secondSlash == -1 ? hashPath.length : firstSlash + secondSlash;
-
-	return hashPath.substring(firstSlash, secondSlash) + URL_SPACE + 'api';
-}
