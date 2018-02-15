e3e1be13572f4295802ff488c63cce276b62b5fb
Elan Hamburger <elan.hamburger@gmail.com>
2018-02-14 23:37:45 -0500

    Added styled-components dependency

--- DevHub/gatsby-config.js
+++ DevHub/gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `Sparkpost Developer`,
     description: `The SparkPost Developer Hub is a collection of resources to help you succeed with SparkPost – the email delivery and analytics service for developers. What will you build?`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`],
 }
