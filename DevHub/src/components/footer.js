d2214fb59b0fe892d223a0d1d038c2dc7b6220e5
Elan Hamburger <elan.hamburger@gmail.com>
2018-03-21 00:12:41 -0400

    Style audit

--- DevHub/src/components/footer.js
+++ DevHub/src/components/footer.js
@@ -3,16 +3,29 @@ import styled from 'styled-components'
 import {color, grayscale} from '../utils/colors'
 import {Container, Row, Column} from '../components/Grid'
 
+const Footer = styled.footer`
+  background-color: ${grayscale('dark')};
+  color: ${grayscale('white')};
+  padding: 38px 0 62px;
+`;
+
 const ColumnHeader = styled.p`
   font-weight: 600;
   margin-top: 24px;
   text-transform: ${props => props.uppercase ? 'uppercase' : 'initial'};
+  font-family: 'Gotham SSm A', 'Gotham SSm B', 'Helvetica', sans-serif;
+  font-size: 14px;
 `;
 
 const ColumnList = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
+`;
+
+const MenuItem = styled.li`
+  line-height: 1.5;
+  font-size: 14px;
 `;
 
 const SubscribeForm = styled.form`
@@ -35,39 +48,39 @@ const SubmitButton = styled.div`
 `;
 
 export default () => (
-  <footer style={{backgroundColor: grayscale('dark'), color: grayscale('white'), padding: '38px 0 62px'}}>
+  <Footer>
     <Container>
       <Row>
         <Column w="2">
           <ColumnHeader uppercase>About</ColumnHeader>
           <ColumnList>
-            <li>About Us</li>
-            <li>Careers</li>
-            <li>Partners</li>
-            <li>Blog</li>
-            <li>Policies</li>
-            <li>Press</li>
+            <MenuItem>About Us</MenuItem>
+            <MenuItem>Careers</MenuItem>
+            <MenuItem>Partners</MenuItem>
+            <MenuItem>Blog</MenuItem>
+            <MenuItem>Policies</MenuItem>
+            <MenuItem>Press</MenuItem>
           </ColumnList>
         </Column>
         <Column w="2">
           <ColumnHeader uppercase>Support</ColumnHeader>
           <ColumnList>
-            <li>Help &amp; Docs</li>
-            <li>Report Abuse</li>
+            <MenuItem>Help &amp; Docs</MenuItem>
+            <MenuItem>Report Abuse</MenuItem>
           </ColumnList>
         </Column>
         <Column w="2">
           <ColumnHeader uppercase>Solutions</ColumnHeader>
           <ColumnList>
-            <li>Enterprise</li>
-            <li>E-Commerce</li>
-            <li>Service Providers</li>
-            <li>Digital Publishing</li>
+            <MenuItem>Enterprise</MenuItem>
+            <MenuItem>E-Commerce</MenuItem>
+            <MenuItem>Service Providers</MenuItem>
+            <MenuItem>Digital Publishing</MenuItem>
           </ColumnList>
         </Column>
         <Column w="1"></Column>
         <Column w="5">
-          <ColumnHeader>Subscribe to our newsletter:</ColumnHeader>
+          <ColumnHeader>Subscribe to our newsletter</ColumnHeader>
           <div>
             <SubscribeForm>
               <EmailInput type="text" placeholder="Enter your email address" />
@@ -77,5 +90,5 @@ export default () => (
         </Column>
       </Row>
     </Container>
-  </footer>
+  </Footer>
 )
