d4f6a026de6216cb842757ed83990aeca54cec44
Elan Hamburger <elan.hamburger@gmail.com>
2018-03-21 20:13:44 -0400

    Added support for separate breakpoints and offsets

--- DevHub/src/components/Grid.js
+++ DevHub/src/components/Grid.js
@@ -41,8 +41,23 @@ const Row = styled.div`
 `;
 
 const Column = styled.div`
-	width: ${props => (props.w / 12 * 100)}%;
+	width: ${props => (props.xs / 12 * 100)}%;
     padding: 0 15px;
+
+    @media (min-width: 768px) {
+        width: ${props => (props.sm / 12 * 100)}%;
+        ${props => props.smOffset && 'margin-left: ' + (props.smOffset / 12 * 100) + '%;'}
+    }
+
+    @media (min-width: 992px) {
+        width: ${props => (props.md / 12 * 100)}%;
+        ${props => props.mdOffset && 'margin-left: ' + (props.mdOffset / 12 * 100) + '%;'}
+    }
+
+    @media (min-width: 1200px) {
+        width: ${props => (props.lg / 12 * 100)}%;
+        ${props => props.lgOffset && 'margin-left: ' + (props.lgOffset / 12 * 100) + '%;'}
+    }
 `;
 
 export {Container, Row, Column}
