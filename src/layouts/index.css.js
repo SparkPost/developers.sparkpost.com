a005fa761b210d3c1806f93edef233e4bf03b740
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-09 11:21:21 -0400

    set up base css

--- src/layouts/index.css.js
+++ src/layouts/index.css.js
@@ -1,18 +1,59 @@
 import { injectGlobal } from 'styled-components'
+import { grayscale } from '../utils/colors'
 import normalize from 'styled-normalize'
 
 
 injectGlobal`
-  @import 'https://cloud.typography.com/6240112/779488/css/fonts.css'
+  @import 'https://cloud.typography.com/6240112/779488/css/fonts.css';
 
   ${normalize}
 
-  * {
+  html {
     box-sizing: border-box;
   }
+  *, *:before, *:after {
+    box-sizing: inherit;
+    position: relative;
+  }
 
-  body {
-    margin: 0;
+  /** Typography */
+
+  html, body {
     font-family: 'Gotham Narrow SSm A', 'Gotham Narrow SSm B', 'Helvetica', sans-serif;
+    font-size: 18px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: ${grayscale('medium')};
+    line-height: 1.15;
+  }
+
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+  }
+
+  h4 {
+    font-size: 1.1rem;
+  }
+
+  h5 {
+    font-size: 1rem;
+  }
+
+  h6 {
+    font-size: .833333333rem;
+  }
+
+  p {
+    font-size: .888888889rem;
+    line-height: 1.5;
   }
 `
