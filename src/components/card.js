fddb29679f01883d62a7ab7ae82daa794d72d190
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-09 11:44:16 -0400

    improved cards styles

--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
-import { color } from '../utils/colors'
+import { color, grayscale } from '../utils/colors'
 
 const Wrapper = styled.div`
   border-top: 6px solid ${props => props.color};
-  height: 120px;
-  background-color: white;
-  padding: 0 22px;
-  margin: 12px 0;
+  background-color: ${grayscale('white')};
+  padding: 1rem 1.25rem 1.5rem;
+  margin-bottom: 1.25rem;
 `
 
-export default props => (
-  <Wrapper color={color(props.color)}>
-    <h2>{props.title}</h2>
-    <p>{props.children}</p>
+const Title = styled.h3`
+  font-size: 1rem;  
+  margin: 0 0 .5rem 0;
+`
+
+const Text = styled.p`
+  margin: 0;
+`
+
+export default ({ title, children, color: c, ...props }) => (
+  <Wrapper color={color(c)} {...props}>
+    <Title>{title}</Title>
+    <Text>{children}</Text>
   </Wrapper>
 )
