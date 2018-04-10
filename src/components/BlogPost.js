c350da47193cffb68a4f74f3c7ef549580ec8765
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-10 12:12:36 -0400

    cleaned up the BlogPost component

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,8 +1,13 @@
 import React from 'React'
 import styled from 'styled-components'
+import { grayscale } from  '../utils/colors'
+import { weight, font, uppercase } from '../utils/fonts'
+
+import Link from './Link'
 
 const Wrapper = styled.div`
-  background-color: white;
+  background-color: ${grayscale('white')};
+  border: 1px solid ${grayscale(7)};
   text-align: center;
   height: 100%;
 `
@@ -10,31 +15,49 @@ const Wrapper = styled.div`
 const Image = styled.div`
   height: 0;
   padding-bottom: 76.1111111%;
-  background-color: red;
+  background-color: ${grayscale(8)};
+  margin: 0 0 .75rem 0;
 `
 
 const Text = styled.div`
-  padding: 0 24px;
+  padding: 0 2rem 2rem;
+`
+
+const Title = styled.h3`
+  font-size: 1rem;
+  margin: 0 0 1.5rem 0;
 `
 
 const ByLine = styled.p`
-  text-transform: uppercase;
-  margin-bottom: 24px;
+  ${uppercase}
+  margin: 0 0 1.5rem 0;
+  font-size: .8rem;
 `
 
-const Author = styled.span`
-  font-weight: 800;
+const Author = styled(Link)`
+  font-weight: ${weight('bold')};
+  color: inherit;
 `
 
-export default () => (
+const ReadMore = styled(Link)`
+  font-weight: ${weight('light')};
+  font-size: .888888889rem;
+
+  i {
+    padding-left: .5rem;
+  }
+`
+
+export default ({ date, author, authorLink, title, description, link }) => (
   <Wrapper>
     <Image />
     <Text>
       <ByLine>
-        Nov. 13, 2017 by <Author>E. Hamburger</Author>
+        {date} by <Author to={authorLink}>{author}</Author>
       </ByLine>
-      <h3>Title of a Blog Post</h3>
-      <p>Preview of a blog post goes here</p>
+      <Title>{title}</Title>
+      <p>{description}</p>
+      <ReadMore to={link}>read more <i className="fa fa-chevron-right"></i></ReadMore>
     </Text>
   </Wrapper>
 )
