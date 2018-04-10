50dba4f2d929ec9dfd938e625da408cbea151e51
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-10 10:12:28 -0400

    formatting

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 import isAbsoluteUrl from 'is-absolute-url'
 
-export default ({ to, ...props }) => (
-  to && isAbsoluteUrl(to) ? <a href={to} {...props} /> : <Link to={to} {...props} />
-)
+export default ({ to, ...props }) =>
+  to && isAbsoluteUrl(to) ? (
+    <a href={to} {...props} />
+  ) : (
+    <Link to={to} {...props} />
+  )
