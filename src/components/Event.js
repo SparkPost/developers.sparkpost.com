817b37c4a9320f42a44c47be5f0f449b191e9761
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-10 11:33:36 -0400

    cleaned up event component

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,49 +1,59 @@
 import React from 'React'
 import styled from 'styled-components'
 import { color, grayscale } from '../utils/colors'
+import { uppercase } from '../utils/fonts'
 
 const Calendar = styled.div`
-	background-color: ${grayscale('white')};
-	border-top: 5px solid ${color('orange')};
-	height: 150px;
-	padding: 24px;
+  background-color: ${grayscale('white')};
+  border-top: 5px solid ${color('orange')};
+  height: 9rem;
+  width: 9rem;
 `
 
 const Month = styled.p`
-	color: ${color('orange')};
-	text-align: center;
-	text-transform: uppercase;
-	margin: 0;
-	font-size: 1.5em;
+  ${uppercase}
+  color: ${color('orange')};
+  text-align: center;
+  margin: .25rem 0 0 0;
+  font-size: 1.25em;
 `
 
 const Day = styled.p`
-	color: ${grayscale('medium')};
-	text-align: center;
-	font-size: 4em;
-	margin: 0;
-	font-weight: 600;
+  color: ${grayscale('medium')};
+  text-align: center;
+  font-size: 4em;
+  margin: 0;
+  font-weight: 600;
 `
 
 const Title = styled.h3`
-	text-align: center;
-	color: white;
+  text-align: center;
+  color: white;
+  font-size: 1.1rem;
+  margin: 1.5rem 0 .5rem 0;
 `
 
-const Info = styled.p`
-	text-align: center;
-	margin: 0;
-	color: white
+const Info = styled.div`
+  text-align: center;
+  margin: 0;
+  color: white;
 `
 
-export default props => (
-  <div>
-    <Calendar>
-      <Month>Jun</Month>
-      <Day>15</Day>
-    </Calendar>
-    <Title>The Incredibles II</Title>
-    <Info>June 15 - 16</Info>
-    <Info>College Park, MD</Info>
-  </div>
-)
+export default ({ start, end, name, location }) => {
+  const startDate = new Date(start)
+  const endDate = new Date(end)
+
+  const startMonth = startDate.getMonth()
+  const startDay = startDate.getDate()
+
+  return (
+    <div>
+      <Calendar>
+        <Month>{startMonth}</Month>
+        <Day>{startDay}</Day>
+      </Calendar>
+      <Title>{name}</Title>
+      <Info>{startMonth} {startDay} - {endDate.getMonth() === startMonth ? '' : endDate.getMonth() } {endDate.getDate()}</Info>
+      <Info>{location}</Info>
+    </div>)
+}
