73b3b5571868a29e6223e7b8ce37dc9ea6e24450
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-10 12:18:54 -0400

    removed unneeded link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import Link from 'gatsby-link'
 import { color, grayscale } from '../utils/colors'
 import { Container, Row, Column } from '../components/Grid'
 import Section from '../components/Section'
