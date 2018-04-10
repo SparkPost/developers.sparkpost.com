7ed289f37c6b3ec906387e92b91cac717eedf5c4
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-10 13:47:04 -0400

    formatting

--- src/components/ClientLibrary.js
+++ src/components/ClientLibrary.js
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 import { grayscale } from '../utils/colors'
 
 const Wrapper = styled.div`
-	border: 1px solid ${grayscale('light')};
-	height: 75px;
-	padding: 8px 12px;
-	margin: 6px 0;
-	line-height: 56px;
+  border: 1px solid ${grayscale('light')};
+  height: 75px;
+  padding: 8px 12px;
+  margin: 6px 0;
+  line-height: 56px;
 `
 
 export default props => <Wrapper>{props.title}</Wrapper>
