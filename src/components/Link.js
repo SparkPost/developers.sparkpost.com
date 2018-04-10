2f770ed444ce9a1ade9fa9296e054046bcc20d9e
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-10 12:13:17 -0400

    added Unstyled version of Link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import GatsbyLink from 'gatsby-link'
 import isAbsoluteUrl from 'is-absolute-url'
+import styled from 'styled-components'
 
-export default ({ to, ...props }) =>
-  to && isAbsoluteUrl(to) ? (
+function Link ({ to = null, ...props }) {
+  return to && isAbsoluteUrl(to) ? (
     <a href={to} {...props} />
   ) : (
-    <Link to={to} {...props} />
+    <GatsbyLink to={to} {...props} />
   )
+}
+
+
+Link.Unstyled = styled(Link)`
+  color: inherit;
+  font-weight: inherit;
+
+  &:hover {
+    text-decoration: none;
+  }
+`
+
+
+
+export default Link
