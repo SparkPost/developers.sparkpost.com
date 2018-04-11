e711b8d7da0d988c89f58e44440cc8d332857013
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-11 14:36:19 -0400

    use Section on community page

--- src/pages/community.js
+++ src/pages/community.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import Section from '../components/Section'
 import { Container, Row, Column } from '../components/Grid'
 import Card from '../components/Card'
 import { grayscale, color } from '../utils/colors'
-
-const Header = styled(Container)`
-  padding-top: 152px;
-  padding-bottom: 48px;
-`
 
 const Title = styled.h1`
   text-align: center;
@@ -43,75 +39,79 @@ const StarsCell = styled.td`
 
 export default () => (
   <div>
-    <Header background={grayscale('light')}>
-      <Title>Open Source Software</Title>
-      <Row>
-        <Column md="8" mdOffset="2">
-          <Subtitle>
-            At SparkPost we rely on Open Source and Standards from hundreds of
-            NPM modules to the SMTP spec. We believe that it is important to
-            give back to the community where we can.
-          </Subtitle>
-        </Column>
-      </Row>
-      <Row>
-        <Column md="4" mdOffset="2">
-          <Card color="blue" title="&lt;heml /&gt;">
-            Comprehensive documentation of our API endpoints &amp; parameters.
-          </Card>
-        </Column>
-        <Column md="4">
-          <Card color="mustard" title="passwordless.academy">
-            Comprehensive documentation of our API endpoints &amp; parameters.
-          </Card>
-        </Column>
-      </Row>
-    </Header>
-    <Container background={grayscale('white')}>
-      <Row>
-        <Column md="8" mdOffset="2">
-          <h2>Our Projects</h2>
-          <Table>
-            <tbody>
-              <tr>
-                <TitleCell>Eriksen</TitleCell>
-                <DescriptionCell>
-                  Lorem ipsem dolor sit amet, consectetur adipiscing elit.
-                </DescriptionCell>
-                <StarsCell>162 ★</StarsCell>
-              </tr>
-              <tr>
-                <TitleCell>email templates</TitleCell>
-                <DescriptionCell>
-                  Lorem ipsem dolor sit amet, consectetur adipiscing elit.
-                </DescriptionCell>
-                <StarsCell>162 ★</StarsCell>
-              </tr>
-              <tr>
-                <TitleCell>postbin</TitleCell>
-                <DescriptionCell>
-                  Lorem ipsem dolor sit amet, consectetur adipiscing elit.
-                </DescriptionCell>
-                <StarsCell>162 ★</StarsCell>
-              </tr>
-              <tr>
-                <TitleCell>php-sparkpost</TitleCell>
-                <DescriptionCell>
-                  Lorem ipsem dolor sit amet, consectetur adipiscing elit.
-                </DescriptionCell>
-                <StarsCell>162 ★</StarsCell>
-              </tr>
-              <tr>
-                <TitleCell>node-sparkpost</TitleCell>
-                <DescriptionCell>
-                  Lorem ipsem dolor sit amet, consectetur adipiscing elit.
-                </DescriptionCell>
-                <StarsCell>162 ★</StarsCell>
-              </tr>
-            </tbody>
-          </Table>
-        </Column>
-      </Row>
-    </Container>
+    <Section light>
+      <Container>
+        <Title>Open Source Software</Title>
+        <Row>
+          <Column md="8" mdOffset="2">
+            <Subtitle>
+              At SparkPost we rely on Open Source and Standards from hundreds of
+              NPM modules to the SMTP spec. We believe that it is important to
+              give back to the community where we can.
+            </Subtitle>
+          </Column>
+        </Row>
+        <Row>
+          <Column md="4" mdOffset="2">
+            <Card color="blue" title="&lt;heml /&gt;">
+              Comprehensive documentation of our API endpoints &amp; parameters.
+            </Card>
+          </Column>
+          <Column md="4">
+            <Card color="mustard" title="passwordless.academy">
+              Comprehensive documentation of our API endpoints &amp; parameters.
+            </Card>
+          </Column>
+        </Row>
+      </Container>
+    </Section>
+    <Section>
+      <Container>
+        <Row>
+          <Column md="8" mdOffset="2">
+            <h2>Our Projects</h2>
+            <Table>
+              <tbody>
+                <tr>
+                  <TitleCell>Eriksen</TitleCell>
+                  <DescriptionCell>
+                    Lorem ipsem dolor sit amet, consectetur adipiscing elit.
+                  </DescriptionCell>
+                  <StarsCell>162 ★</StarsCell>
+                </tr>
+                <tr>
+                  <TitleCell>email templates</TitleCell>
+                  <DescriptionCell>
+                    Lorem ipsem dolor sit amet, consectetur adipiscing elit.
+                  </DescriptionCell>
+                  <StarsCell>162 ★</StarsCell>
+                </tr>
+                <tr>
+                  <TitleCell>postbin</TitleCell>
+                  <DescriptionCell>
+                    Lorem ipsem dolor sit amet, consectetur adipiscing elit.
+                  </DescriptionCell>
+                  <StarsCell>162 ★</StarsCell>
+                </tr>
+                <tr>
+                  <TitleCell>php-sparkpost</TitleCell>
+                  <DescriptionCell>
+                    Lorem ipsem dolor sit amet, consectetur adipiscing elit.
+                  </DescriptionCell>
+                  <StarsCell>162 ★</StarsCell>
+                </tr>
+                <tr>
+                  <TitleCell>node-sparkpost</TitleCell>
+                  <DescriptionCell>
+                    Lorem ipsem dolor sit amet, consectetur adipiscing elit.
+                  </DescriptionCell>
+                  <StarsCell>162 ★</StarsCell>
+                </tr>
+              </tbody>
+            </Table>
+          </Column>
+        </Row>
+      </Container>
+    </Section>
   </div>
 )
