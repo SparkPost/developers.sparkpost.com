2db5f56f3b633c8fe5a91c4889f453a438fe1fb8
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-11 14:37:53 -0400

    added netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `The SparkPost Developer Hub is a collection of resources to help you succeed with SparkPost – the email delivery and analytics service for developers. What will you build?`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
      {
