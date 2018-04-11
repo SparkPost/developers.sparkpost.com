ab9f8abe72aafaf9945d7c781c219002a1d15f15
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-11 09:33:23 -0400

    added status icon and link

--- src/components/Header/SecondaryNav.js
+++ src/components/Header/SecondaryNav.js
@@ -1,22 +1,69 @@
 import React from 'react'
 import Link from '../Link'
 import styled from 'styled-components'
+import scriptjs from 'scriptjs'
 import { grayscale, color } from '../../utils/colors'
+import { weight } from '../../utils/fonts'
+
 
 import NavLink from './NavLink'
 
-const SecondaryNav = styled.div`
+const Nav = styled.div`
   text-align: right;
 `
 
 const SecondaryNavLink = NavLink.extend`
-  font-weight: 400;
+  font-weight: ${weight('light')};
 `
 
-export default () => (
-  <SecondaryNav>
-    <SecondaryNavLink>Blog</SecondaryNavLink>
-    <SecondaryNavLink>Support</SecondaryNavLink>
-    <SecondaryNavLink>Your Dashboard</SecondaryNavLink>
-  </SecondaryNav>
-)
+const colorMap = {
+  loading: grayscale(7),
+  none: color('green'),
+  minor: color('mustard'),
+  major: color('orange'),
+  critical: color('alert')
+}
+
+const StatusIcon = styled.div`
+  display: inline-block;
+  width: .5rem;
+  height: .5rem;
+  border-radius: 50%;
+  background: ${props => colorMap[props.status]};
+  margin-right: .25rem;
+  transition: .25s;
+`
+
+class SecondaryNav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { status: 'loading' }
+  }
+
+  componentDidMount() {
+    scriptjs('https://cdn.statuspage.io/se-v2.js', () => {
+      var sp = new StatusPage.page({ page: '7ky1q6zd3fyp'})
+
+      sp.summary({
+        success: (data) => {
+          this.setState({ status: data.status.indicator })
+        }
+      })
+    })
+  }
+
+  render() {
+
+    return (
+      <Nav>
+        <SecondaryNavLink to="https://status.sparkpost.com"><StatusIcon status={this.state.status} /> Status</SecondaryNavLink>
+        <SecondaryNavLink to="https://sparkpost.com/blog">Blog</SecondaryNavLink>
+        <SecondaryNavLink to="https://sparkpost.com/docs">Support</SecondaryNavLink>
+        <SecondaryNavLink to="https://app.sparkpost.com/">Your Dashboard</SecondaryNavLink>
+      </Nav>
+    )
+  }
+
+}
+
+export default SecondaryNav
