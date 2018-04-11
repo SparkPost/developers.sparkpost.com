0fb64958a715b243d35761c87eb19e480a948900
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-11 11:13:24 -0400

    added ActionLink component

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,9 @@ import GatsbyLink from 'gatsby-link'
 import isAbsoluteUrl from 'is-absolute-url'
 import styled from 'styled-components'
 
+import { color, grayscale } from '../utils/colors'
+import { weight } from '../utils/fonts'
+
 function Link ({ to = null, onClick, ...props }) {
   return (to && isAbsoluteUrl(to)) || onClick ? (
     <a href={to} onClick={onClick} {...props} />
@@ -18,6 +21,23 @@ Link.Unstyled = styled(Link)`
 
   &:hover {
     text-decoration: none;
+  }
+`
+
+Link.Action = styled(Link.Unstyled)`
+  background: transparent;
+  border: 0;
+  font-size: .833333333rem;
+  padding: .3rem .66667rem;
+  color: ${color('orange')};
+  font-weight: ${weight('medium')};
+  cursor: pointer;
+  outline: 0;
+  display: inline-block;
+
+  &:hover, &:active, &:focus {
+    color: ${color('orangeDark')};
+    background: ${grayscale('light')};
   }
 `
 
