72d219ea53a23c7b4b2bff31614726f005b86a98
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-12 11:23:45 -0400

    started building libraries pages

--- src/pages/libraries.js
+++ src/pages/libraries.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Container, Row, Column } from '../components/Grid'
-import { grayscale, color } from '../utils/colors'
 import styled from 'styled-components'
+import { grayscale, color } from '../utils/colors'
+import { Container, Row, Column } from '../components/Grid'
+import Section from '../components/Section'
 import BlogPost from '../components/BlogPost'
+import ClientLibrary from '../components/ClientLibrary'
 
 const Title = styled.h1`
   text-align: center;
@@ -17,47 +19,130 @@ const CodeBlock = styled.code`
   font-family: 'Monaco';
 `
 
-const SectionTitle = styled.h2`
-  text-align: center;
-  color: ${props => (props.dark ? grayscale('white') : grayscale('dark'))};
-`
 
 export default () => (
   <div>
-    <Container background={grayscale('light')}>
-      <Title>SparkPost and Node.js</Title>
-      <Row>
-        <Column md="8" mdOffset="2">
-          <CodeBlock>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </CodeBlock>
-        </Column>
-      </Row>
-    </Container>
-    <Container background={grayscale('white')} />
-    <Container background={grayscale('light')}>
-      <SectionTitle>Post About Node</SectionTitle>
-      <Row>
-        <Column md="10" mdOffset="1">
+    <Section light>
+      <Container>
+        <Title>Libraries & Integrations</Title>
+      </Container>
+    </Section>
+    <Section>
+      <Container>
+        <h2>Client Libraries</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, impedit.</p>
+        <Container>
           <Row>
-            <Column md="4">
-              <BlogPost />
-            </Column>
-            <Column md="4">
-              <BlogPost />
-            </Column>
-            <Column md="4">
-              <BlogPost />
+            <Column md={8} mdOffset={2}>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Node.js" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="PHP" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Python" />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Java" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Elixir" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="NodeMailer" />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Go" />
+                </Column>
+              </Row>
             </Column>
           </Row>
-        </Column>
-      </Row>
-    </Container>
+        </Container>
+      </Container>
+    </Section>
+    <Section light>
+      <Container>
+        <h2>Integrations</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, impedit.</p>
+        <Container>
+          <Row>
+            <Column md={8} mdOffset={2}>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Node.js" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="PHP" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Python" />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Java" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Elixir" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="NodeMailer" />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Go" />
+                </Column>
+              </Row>
+            </Column>
+          </Row>
+        </Container>
+      </Container>
+    </Section>
+    <Section>
+      <Container>
+        <h2>Marketplaces</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, impedit.</p>
+        <Container>
+          <Row>
+            <Column md={8} mdOffset={2}>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Node.js" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="PHP" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Python" />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Java" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Elixir" />
+                </Column>
+                <Column md={4}>
+                  <ClientLibrary img="" title="NodeMailer" />
+                </Column>
+              </Row>
+              <Row>
+                <Column md={4}>
+                  <ClientLibrary img="" title="Go" />
+                </Column>
+              </Row>
+            </Column>
+          </Row>
+        </Container>
+      </Container>
+    </Section>
   </div>
 )
---
+++ src/pages/node.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Container, Row, Column } from '../components/Grid'
+import { grayscale, color } from '../utils/colors'
+import styled from 'styled-components'
+import BlogPost from '../components/BlogPost'
+
+const Title = styled.h1`
+  text-align: center;
+  color: ${color('gray')};
+`
+
+const CodeBlock = styled.code`
+  border-top: 4px solid ${color('orange')};
+  display: block;
+  background-color: white;
+  padding: 8px;
+  font-family: 'Monaco';
+`
+
+const SectionTitle = styled.h2`
+  text-align: center;
+  color: ${props => (props.dark ? grayscale('white') : grayscale('dark'))};
+`
+
+export default () => (
+  <div>
+    <Container background={grayscale('light')}>
+      <Title>SparkPost and Node.js</Title>
+      <Row>
+        <Column md="8" mdOffset="2">
+          <CodeBlock>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </CodeBlock>
+        </Column>
+      </Row>
+    </Container>
+    <Container background={grayscale('white')} />
+    <Container background={grayscale('light')}>
+      <SectionTitle>Post About Node</SectionTitle>
+      <Row>
+        <Column md="10" mdOffset="1">
+          <Row>
+            <Column md="4">
+              <BlogPost />
+            </Column>
+            <Column md="4">
+              <BlogPost />
+            </Column>
+            <Column md="4">
+              <BlogPost />
+            </Column>
+          </Row>
+        </Column>
+      </Row>
+    </Container>
+  </div>
+)
