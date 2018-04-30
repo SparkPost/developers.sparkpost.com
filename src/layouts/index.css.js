978fff39f5ba1fbdf0622693a385eeda0204e30b
Avi Goldman <avrahamymgoldman@gmail.com>
2018-04-30 16:09:40 -0700

    improvements to global styles

--- src/layouts/index.css.js
+++ src/layouts/index.css.js
@@ -1,6 +1,6 @@
 import { injectGlobal } from 'styled-components'
 import { color, grayscale } from '../utils/colors'
-import { font, weight } from '../utils/fonts'
+import { font, weight, monospace } from '../utils/fonts'
 import normalize from 'styled-normalize'
 
 injectGlobal`
@@ -11,10 +11,12 @@ injectGlobal`
   html {
     box-sizing: border-box;
     background: ${grayscale('light')};
+    height: 100%;
   }
 
   body {
     background: ${grayscale('white')};
+    min-height: 100%;
   }
 
   *, *:before, *:after {
@@ -24,27 +26,34 @@ injectGlobal`
 
   /** Typography */
 
-  html, body {
+  html {
     font-family: ${font('primary')};
     font-weight: ${weight('light')};
     font-size: 18px;
+    text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     color: ${grayscale('medium')};
     line-height: 1.15;
   }
 
+  body {
+    font-size: .888888889rem;
+  }
+  
+  h1, h2, h3, h4, h5, h6, p {
+    margin: 0 0 1rem 0;
+  }
 
   h1 {
-    font-size: 2rem;
+    font-size: 1.666666667rem;
   }
 
   h2 {
-    font-size: 1.5rem;
+    font-size: 1.333333333rem;
   }
 
   h3 {
-    font-size: 1.1rem;
+    font-size: 1.166666667rem;
   }
 
   h4 {
@@ -52,16 +61,15 @@ injectGlobal`
   }
 
   h5 {
-    font-size: 1rem;
+    font-size: .888888889rem;
   }
 
   h6 {
     font-size: .833333333rem;
   }
 
-  p {
-    font-size: .888888889rem;
-    line-height: 1.5;
+  p, td, tr {
+    line-height: 1.65;
   }
 
   a {
@@ -86,4 +94,74 @@ injectGlobal`
   .textRight {
     text-align: right;
   }  
+
+  b, strong {
+    font-weight: ${weight('bold')};
+  }
+
+  code {
+    ${monospace}
+    background: ${grayscale('light')};
+    font-weight: 500;
+    padding: 2px 4px;
+    border-radius: 2px;
+    border: 1px solid ${grayscale(8)};
+  }
+
+  pre {
+    display: block;
+    padding: 1rem;
+    word-break: break-all;
+    word-wrap: break-word;
+    color: inherit;
+    background-color: ${grayscale('light')};
+    border-radius: 4px;
+    overflow: auto;
+    line-height: 1.25;
+
+    code {
+      background: transparent;
+      color: inherit;
+      overflow: auto;
+      border: 0;
+      white-space: inherit;
+    }
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+    margin-bottom: 2rem;
+
+    th, td {
+      border-bottom: 1px solid ${grayscale(8)};
+      text-align: left;
+      padding: 0.83333rem;
+      vertical-align: top;
+
+      &:first-child {
+        padding-left: 0;
+      }
+
+      &:last-child {
+        padding-right: 0;
+      }
+    }
+
+    th {
+      border-bottom-width: 2px;
+      padding: 0.5rem;
+    }
+  } 
+
+  img {
+    max-width: 100%;
+  } 
+
+  hr {
+    border: 0;
+    border-bottom: 1px solid ${grayscale(8)};
+    margin: 2rem 0;
+  }
 `
