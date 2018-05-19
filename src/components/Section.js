8214674993604a5cd0ccf12797e53d3bddf5ff3f
Avi Goldman <avrahamymgoldman@gmail.com>
2018-05-18 20:59:34 -0700

    made section size configurable

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,9 +4,25 @@ import { grayscale } from '../utils/colors'
 
 import hex from '../assets/hex.png'
 
+const paddingMap = {
+  none: { top: 0, bottom: 0 },
+  xs:   { top: 2, bottom: 3 },
+  sm:   { top: 3, bottom: 4 },
+  md:   { top: 4, bottom: 5 },
+  lg:   { top: 5, bottom: 6 }
+}
+
 const Section = styled.section`
-  padding: 4rem 0 5rem 0;
-  border-bottom: 1px solid ${grayscale(7)};
+
+  ${({ spacing = 'md', spacingTop, spacingBottom }) => {
+    const top = paddingMap[spacingTop || spacing].top
+    const bottom = paddingMap[spacingBottom || spacing].bottom
+
+    return `padding: ${top}rem 0 ${bottom}rem;`
+
+  }}
+
+  ${props => !props.borderless && `border-bottom: 1px solid ${grayscale(7)};`}
 
   ${props => props.dark && css`
     background-color: ${grayscale('dark')};
