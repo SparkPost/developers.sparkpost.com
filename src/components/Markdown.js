9ea81077effe0e6acf4d0c5a49ae6604883324e3
Avi Goldman <avrahamymgoldman@gmail.com>
2018-05-26 09:51:50 -0700

    created markdown component

---
+++ src/components/Markdown.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { castArray } from 'lodash'
+import unified from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeRaw from 'rehype-raw'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeReact from 'rehype-react'
+
+const markdownProcessor = unified()
+  .use(remarkParse)
+  .use(remarkRehype, { allowDangerousHTML: true })
+  .use(rehypeRaw)
+  .use(rehypeHighlight)
+
+const baseMarkdownProcessor = markdownProcessor().use(rehypeReact, {
+  createElement: React.createElement
+})
+
+
+/** 
+ * Component to render markdown to React
+ *
+ * <Markdown components={{
+ *   h1: MyH1,
+ *   custom-component: CustomComponent
+ * }}>
+ * # Heading
+ *
+ * [Sign up](https://app.sparkpost.com/join) for SparkPost..
+ * </Markdown>
+ */
+export default ({ children, components, ...props }) => {
+  const markdown = castArray(children).join('')
+
+  let processor = baseMarkdownProcessor
+
+  /* create a custom processor if given components */
+  if (components) {
+    processor = markdownProcessor().use(rehypeReact, {
+      createElement: React.createElement,
+      components
+    })
+  }
+
+  return (
+    <div {...props}>{processor.processSync(markdown).contents}</div>
+  )
+}
