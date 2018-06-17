bd892700afe98c700c39a33c2411644acd70d404
Avi Goldman <avrahamymgoldman@gmail.com>
2018-06-17 10:33:13 -0700

    formatting

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,9 @@ const axios = require('axios')
 const fs = require('fs')
 const { flatten } = require('lodash')
 
+/**
+ * Add the image links to wordpress posts
+ */
 exports.onCreateNode = async ({ node, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
 
@@ -21,6 +24,9 @@ exports.onCreateNode = async ({ node, boundActionCreators }) => {
   }
 }
 
+/**
+ * create the API reference pages
+ */
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
 
