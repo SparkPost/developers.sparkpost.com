3f417fcae0483f65ed6424c164bca01357141dfa
Avi Goldman <avrahamymgoldman@gmail.com>
2018-06-25 11:40:28 -0700

    prettier

--- src/components/Markdown/index.js
+++ src/components/Markdown/index.js
@@ -17,7 +17,7 @@ const markdownProcessor = unified()
 const baseMarkdownProcessor = markdownProcessor().use(rehypeReact, {
   createElement: React.createElement,
   components: {
-    a: ({ href, ...props }) => (<Link to={href} {...props} />),
+    a: ({ href, ...props }) => <Link to={href} {...props} />,
   },
 })
 
