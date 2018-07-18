7829c4e3eedb0c23b3ff7a8e271490432468b05f
Avi Goldman <avrahamymgoldman@gmail.com>
2018-07-18 11:09:11 -0700

    added in nodes for webhook categories and events

--- plugins/gatsby-source-sparkpost-api/gatsby-node.js
+++ plugins/gatsby-source-sparkpost-api/gatsby-node.js
@@ -1,5 +1,5 @@
 /**
- * Create MessageEvent nodes
+ * Create MessageEvent, WebhookCategory, and WebhookEvent nodes
  */
 
 const crypto = require('crypto')
@@ -40,10 +40,88 @@ exports.sourceNodes = async ({ actions, createNodeId }) => {
 
     createNode(nodeData)
   })
+
+  // Create webhook event nodes
+  const { data: { results: webhookDocumentation } } = await axios.get('https://api.sparkpost.com/api/v1/webhooks/events/documentation')
+  const { data: { results: webhookSamples } } = await axios.get('https://api.sparkpost.com/api/v1/webhooks/events/samples')
+
+  const webhookSamplesMap = keyBy(webhookSamples, (sample) => {
+    const category = Object.keys(sample.msys)[0]
+
+    return sample.msys[category].type
+  })
+
+  const processWebhookCategory = (category) => {
+    const nodeId = createNodeId(`sparkpost-webhook-category-${category.name}`)
+    const nodeContent = JSON.stringify(category)
+    const nodeContentDigest = crypto
+      .createHash('md5')
+      .update(nodeContent)
+      .digest('hex')
+
+    return Object.assign({}, category, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `WebhookCategory`,
+        content: nodeContent,
+        contentDigest: nodeContentDigest
+      }
+    })
+  }
+
+  const processWebhookEvent = (event) => {
+    const attributes = event.event
+    const name = attributes.type.sampleValue
+    const sample = webhookSamplesMap[name]
+    const nodeId = createNodeId(`sparkpost-webhook-event-${name}`)
+    const nodeObject = {
+      name, attributes, sample, description: event.description, display_name: event.display_name
+    }
+    const nodeContent = JSON.stringify(nodeObject)
+    const nodeContentDigest = crypto
+      .createHash('md5')
+      .update(nodeContent)
+      .digest('hex')
+
+    return {
+      id: nodeId,
+      parent: null,
+      children: [],
+      name: nodeObject.name,
+      description: nodeObject.description,
+      display_name: nodeObject.display_name,
+      internal: {
+        type: `WebhookEvent`,
+        content: nodeContent,
+        contentDigest: nodeContentDigest
+      }
+    }
+  }
+
+  map(webhookDocumentation, (category, name) => {
+    const categoryNode = processWebhookCategory({
+      name,
+      description: category.description,
+      display_name: category.display_name
+    })
+
+    createNode(categoryNode)
+
+    map(category.events, (event) => {
+      const eventNode = processWebhookEvent(event)
+      eventNode.parent = categoryNode.id
+      eventNode.category = categoryNode.name
+
+      createNode(eventNode)
+      createParentChildLink({ parent: categoryNode, child: eventNode })
+    })
+  })
 }
 
 exports.setFieldsOnGraphQLNodeType = async ({ type }) => {
-  if (type.name !== `MessageEvent`) {
+  if (type.name !== `MessageEvent` && type.name !== `WebhookEvent`) {
     return {}
   }
 
