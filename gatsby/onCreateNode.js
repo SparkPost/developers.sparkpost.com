ecf70f1318f6ed96ba69cfd7ac6ca17a0da475da
Avi Goldman <avrahamymgoldman@gmail.com>
2018-08-09 20:43:10 -0600

    attach path and file in onCreateNode

--- gatsby/onCreateNode.js
+++ gatsby/onCreateNode.js
@@ -1,14 +1,15 @@
-/**
- * Add the image links to wordpress posts since it takes way too much time to download them all
- */
-
 'use strict'
 
 const axios = require('axios')
+const { resolve } = require('path')
+const tableOfContents = require(`../content/api/table-of-contents.json`)
 
-module.exports = async ({ node, actions }) => {
+module.exports = async ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
+  /**
+   * Add the image links to wordpress posts since it takes way too much time to download them all
+   */
   if (node.internal.type === `wordpress__POST`) {
     let mediaUrl = ''
 
@@ -23,5 +24,26 @@ module.exports = async ({ node, actions }) => {
     }
 
     createNodeField({ node, name: `media`, value: mediaUrl })
+  }
+
+  /**
+   * Add the path and file to ApiBlueprint nodes
+   */
+  if (node.internal.type === 'ApiBlueprint') {
+    const { name: fileName, base: file } = getNode(node.parent)
+
+    // add path
+    createNodeField({
+      node,
+      name: `path`,
+      value: `/api/${fileName === 'index' ? '' : `${fileName}/`}`
+    })
+
+    // add file
+    createNodeField({
+      node,
+      name: `file`,
+      value: file
+    })
   }
 }
