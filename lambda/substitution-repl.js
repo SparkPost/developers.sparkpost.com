55bfde1b48a25872126e64cee90c9c60d7753f95
Avi Goldman <avrahamymgoldman@gmail.com>
2018-08-11 18:06:48 -0700

    added support for dynamic content

--- lambda/substitution-repl.js
+++ lambda/substitution-repl.js
@@ -4,44 +4,109 @@ const sparkpost = new SparkPost()
 
 const PREVIEW_TEMPLATE_ID = `substitution-repl`
 
-exports.handler = async function(event, context, callback) {
+// for updating the draft in dynamic previews
+const generateTemplateContent = (html) => ({
+  html,
+  "subject": "subject",
+  "from": {
+      "email": "noreply@challenge.sparkpost.com",
+      "name": "SparkPost"
+  }
+})
+
+/**
+ * Given html and substitution_data, this function renders a preview of the
+ * combined result via the SparkPost API.
+ *
+ * There are two types of previews: dynamic and simple.
+ *
+ * Dynamic previews are previews that have dynamic content. Dynamic content
+ * is content passed in through substitution_data.dynamic_html or
+ * substitution_data.dynamic_plain which can contain expressions to
+ * be compiled (.i.e. "hello {{name}}").
+ *
+ * Simple previews are straight complication without any dynamic content.
+ *
+ * We use dynamic content to render the simple previews to avoid updating
+ * the template unnecessarily. However, for dynamic previews to work properly
+ * we have to update the template draft and _then_ preview the new draft.
+ */
+exports.handler = async function(event, context) {
   const body = JSON.parse(event.body)
+  const { html, substitution_data: substitution_data_string } = body
 
-  if (!body.hasOwnProperty('html') || !body.hasOwnProperty('substitution_data')) {
-    // mimic the SparkPost error response
-    return error('html or substitution_data is missing', callback)
+  if (!html || !substitution_data_string) {
+    return error('html or substitution_data is missing')
   }
 
+  // convert substitution_data_string into a JSON object
+  let substitution_data
   try {
-    sparkpost.templates.preview(PREVIEW_TEMPLATE_ID, merge({
-      substitution_data: JSON.parse(body.substitution_data)
-    }, {
-      substitution_data: {
-        dynamic_html: {
-          html: body.html
-        },
-      }
-    }))
-    .then((response) => done(response, callback))
-    .catch((response) => done(response, callback))
+    substitution_data = JSON.parse(substitution_data_string)
+  } catch(e) {
+    return error(e.message)
   }
-  catch(e) {
-    return error(e.message, callback)
+
+  if (/{{\s*render_dynamic_content.*}}/.test(html)) {
+    return dynamicPreview({ html, substitution_data })
+  } else {
+    return simplePreview({ html, substitution_data })
   }
+
 }
 
 
-function done(response, callback) {
+/**
+ * Update the template draft and then preview it with the given
+ * substitution_data to properly handle any dynamic content.
+ */
+function dynamicPreview({ html, substitution_data }) {
+  return sparkpost.templates.update(PREVIEW_TEMPLATE_ID, {
+    content: generateTemplateContent(html)
+  })
+  .then(() => {
+    return sparkpost.templates.preview(PREVIEW_TEMPLATE_ID, {
+      substitution_data,
+      draft: true
+    })
+  })
+  .then((response) => done(response))
+  .catch((response) => done(response))
+}
+
+
+/**
+ * Preview the published template which will render the dynamic
+ * content in substitution_data.dynamic_html.html
+ */
+function simplePreview({ html, substitution_data }) {
+  return sparkpost.templates.preview(PREVIEW_TEMPLATE_ID, merge({
+    substitution_data
+  }, {
+    substitution_data: {
+      dynamic_html: { html },
+    }
+  }))
+  .then((response) => done(response))
+  .catch((response) => done(response))
+}
+
+/**
+ * Passthrough for SparkPost API responses
+ */
+function done(response) {
   // pass the response back to the client
-  callback(null, {
+  return Promise.resolve({
     statusCode: 200,
     body: JSON.stringify(response, null, 2)
   })
 }
 
-
-function error(message, callback) {
-  callback(null, {
+/**
+ * Function for returning SparkPost-like errors
+ */
+function error(message) {
+  return Promise.resolve({
     statusCode: 200,
     body: JSON.stringify({
       errors: [
