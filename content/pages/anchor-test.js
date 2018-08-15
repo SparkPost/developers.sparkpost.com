568ac1fd81238d4c134ac311d708a1fc34c04e5e
Avi Goldman <avrahamymgoldman@gmail.com>
2018-08-15 15:45:58 -0700

    test page

---
+++ content/pages/anchor-test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <a href="#one">one</a>
+      <a href="#two">two</a>
+      <a href="#three">three</a>
+    </div>
+  )
+}
