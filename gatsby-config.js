e903f6e42ed0be472c3c0c97afdb946c9b55b39a
Avi Goldman <avrahamymgoldman@gmail.com>
2018-09-29 10:28:08 -0400

    added mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,66 +1,83 @@
-'use strict'
+"use strict";
 
-const { flatten } = require('lodash')
-const queries = require('./algolia-queries.js')
-const proxy = require('http-proxy-middleware')
+const { flatten } = require("lodash");
+const queries = require("./algolia-queries.js");
+const proxy = require("http-proxy-middleware");
 
 // Environment variables prefixed by "GATSBY" are accessible in src/ files
-process.env.GATSBY_ACTIVE_ENV = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV
+process.env.GATSBY_ACTIVE_ENV =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV;
 
 module.exports = {
   siteMetadata: {
     title: `SparkPost Developers`,
-    description: `SparkPost developer resources including documentation, API reference, and client libraries.`,
+    description: `SparkPost developer resources including documentation, API reference, and client libraries.`
   },
   developMiddleware: app => {
     app.use(
-      '/.netlify/functions/',
+      "/.netlify/functions/",
       proxy({
-        target: 'http://localhost:9000',
+        target: "http://localhost:9000",
         pathRewrite: {
-          '/.netlify/functions/': '',
-        },
+          "/.netlify/functions/": ""
+        }
       })
-    )
+    );
   },
   plugins: flatten([
-    process.env.GATSBY_ACTIVE_ENV !== 'docs' ? [
-      {
-        resolve: `gatsby-plugin-page-creator`,
-        options: {
-          path: `${__dirname}/content/pages`,
-        },
-      },
-      {
-        resolve: `gatsby-source-wordpress`,
-        options: {
-          baseUrl: 'sparkpost.com',
-          protocol: 'https',
-          hostingWPCOM: false,
-          useACF: false,
-          excludedRoutes: [ '**/oembed/**', '**/akismet/**', '**/yoast/**' , '**/users/**', '**/settings', '**/pages', '**/yst_prominent_words', '**/comments', '**/statuses', '**/media' ]
-        }
-      }
-    ] : [],
-    process.env.GATSBY_ACTIVE_ENV === 'publish' ? [
-      {
-        resolve: `gatsby-plugin-algolia`,
-        options: {
-          appId: 'SFXAWCYDV8',
-          apiKey: process.env.ALGOLIA_TOKEN,
-          queries
-        }
-      }
-    ] : [],
+    process.env.GATSBY_ACTIVE_ENV !== "docs"
+      ? [
+          {
+            resolve: `gatsby-plugin-page-creator`,
+            options: {
+              path: `${__dirname}/content/pages`
+            }
+          },
+          {
+            resolve: `gatsby-source-wordpress`,
+            options: {
+              baseUrl: "sparkpost.com",
+              protocol: "https",
+              hostingWPCOM: false,
+              useACF: false,
+              excludedRoutes: [
+                "**/oembed/**",
+                "**/akismet/**",
+                "**/yoast/**",
+                "**/users/**",
+                "**/settings",
+                "**/pages",
+                "**/yst_prominent_words",
+                "**/comments",
+                "**/statuses",
+                "**/media"
+              ]
+            }
+          }
+        ]
+      : [],
+    process.env.GATSBY_ACTIVE_ENV === "publish"
+      ? [
+          {
+            resolve: `gatsby-plugin-algolia`,
+            options: {
+              appId: "SFXAWCYDV8",
+              apiKey: process.env.ALGOLIA_TOKEN,
+              queries
+            }
+          }
+        ]
+      : [],
     `gatsby-transformer-json`,
     `gatsby-transformer-api-blueprint`,
+    `gatsby-mdx`,
     `gatsby-source-sparkpost-api`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `${__dirname}/content/`,
-      },
+        path: `${__dirname}/content/`
+      }
     },
     {
       resolve: `gatsby-plugin-styled-components`,
@@ -72,15 +89,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: {
-        id: 'GTM-WN7C84',
-        includeInDevelopment: false,
+        id: "GTM-WN7C84",
+        includeInDevelopment: false
       }
     },
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
         color: `#fa6423`,
-        showSpinner: false,
+        showSpinner: false
       }
     },
     `gatsby-plugin-offline`,
@@ -92,9 +109,9 @@ module.exports = {
         headers: {
           // Disable cache for Gatsby's default service worker file path
           // suggested by https://www.netlify.com/blog/2018/06/28/5-pro-tips-and-plugins-for-optimizing-your-gatsby---netlify-site/
-          "/sw.js": [ "Cache-Control: no-cache" ]
-        },
+          "/sw.js": ["Cache-Control: no-cache"]
+        }
       }
     }
   ])
-}
+};
