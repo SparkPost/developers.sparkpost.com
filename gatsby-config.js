15ebfb33327407affea0b8431556ccbd04631e8c
Avi Goldman <avrahamymgoldman@gmail.com>
2019-03-29 07:53:06 -0700

    images

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,22 @@ module.exports = {
         },
       }
     },
-    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 1000,
+          },
+        },
+      ],
+    },
     // {
     //   resolve: `gatsby-mdx`,
     //   options: {
