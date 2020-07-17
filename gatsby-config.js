06db2a8ccb3a79fc8b8e525e69035e7bed862d52
Brian Kemper <brian.kemper@sparkpost.com>
2020-07-17 12:45:33 -0400

    AC-1565 Uninstalled gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,6 @@ module.exports = {
         showSpinner: false,
       }
     },
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cache`,
     `gatsby-plugin-lodash`,
     `gatsby-plugin-react-helmet`,
